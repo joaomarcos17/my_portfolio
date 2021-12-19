1281e132de656353d0f65ed79576ba4eb1ebcf9b
JoaoMarcos17 <joaomarcoscalacia@hotmail.com>
2021-12-19 08:58:01 +0000

    my template

---
+++ script.js
@@ -0,0 +1,15 @@
+// test script
+
+var song = "Waiting for tonight";
+var singer = "Jennifer Lopez";
+
+// alert the var
+
+// alert("the song is " + song + " by " + singer);
+
+
+// newPara = document.createElement('p');
+
+// document.querySelector('footer').appendChild(newPara);
+
+// newPara.innerHTML = "&copy; Joao Marcos " + new Date().getFullYear();
